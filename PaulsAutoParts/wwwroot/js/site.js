88d8d873d4c20e66032e59541513cc1b98a79983
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2022-11-26 15:03:27 +0600

    Display Please Wait Message, Gray Out Background Using CSS, jQuery, and HTML to show Visual Feedback When Server Runs a Long Operation

--- PaulsAutoParts/wwwroot/js/site.js
+++ PaulsAutoParts/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+'use strict';
+
+// ************************************
+// Create a Closure
+// ************************************
+var mainController = (function () {
+    // ************************************
+    // Private Functions
+    // ************************************
+    function pleaseWait() {
+        $("#pleaseWait").removeClass("d-none");
+        $("header").addClass("pleaseWaitArea");
+        $("main").addClass("pleaseWaitArea");
+        $("footer").addClass("pleaseWaitArea");
+    }
+
+    // ************************************
+    // Public Functions
+    // ************************************
+    return {
+        "pleaseWait": pleaseWait
+    }
+})();
